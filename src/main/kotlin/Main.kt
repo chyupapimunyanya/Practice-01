84dc18d42e014bd65260e30b64f4101291aa7853
chyupapimunyanya <bykadorov2010@gmail.com>
2023-09-07 13:49:43 +0300

    pra

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,49 @@
+fun main(args: Array<String>) {
+
+    //зд1
+    val exercises: Int = 13
+var exercisesSolved: Int =0
+    exercisesSolved++
+    //конец первого начало второго
+    val personAge: Int = 18
+    // конец второго и начало третьего
+    var age1: Double = 18.0
+    age1= (18.0 + 30.0)/2//24
+    //конец третьего и начало четвертого
+    val testNumber: Int = 999
+    val evenOdd: Int = testNumber//меняет значение от 1 до 0
+    //конец четвертого начало пятого
+    var answer: Int=0
+    answer +=1
+    answer +=10
+    answer *= 10
+    answer=answer shr 3//13
+
+    //конец пятого начало шестого
+    var age: Int
+    age= 16
+
+    age=30
+
+    //конец шестого начало седьмого
+
+    //1
+   //4610
+    //2
+   //5600
+    //3
+   //4601
+
+    //кц седьмого, восьмое
+    ((5*3)-((4/2)*2))
+    //кц восьмого нач девятого
+    val a: Double = 7.0
+    val b: Double = 8.0
+    val average: Double =(a+b)/2//13.5
+    //кц девятого нач десятого
+    val fahrenheit: Double=90.8
+    val celsius: Double= (fahrenheit-32)/1.8 //32.66666666666
+//конец десятого
+
+
+}
